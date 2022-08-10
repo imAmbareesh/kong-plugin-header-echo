cd880753d2f076d4ec0b2c5e37ea8013cf5b4025
imAmbareesh <109645071+imAmbareesh@users.noreply.github.com>
2022-08-10 12:21:37 +0530

    Add files via upload

---
+++ kong/plugins/header-echo/schema.lua
@@ -0,0 +1,16 @@
+return {
+    no_consumer = true, -- This means our plugin will not apply to specific service consumers
+    fields      = {
+      requestHeader = {
+        type     = "string",
+        required = false,
+        default  = "X-Request-Echo"
+      },
+      responseHeader = {
+        type     = "string",
+        required = false,
+        default  = "X-Response-Echo"
+      }
+    }
+  }
+  
